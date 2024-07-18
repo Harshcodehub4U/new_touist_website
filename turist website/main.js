584be9f22f7e7c16b92322389e5a18e05663b023
Harsh <165124059+Harshcodehub4U@users.noreply.github.com>
2024-07-18 19:18:34 +0530

    Add files via upload

---
+++ turist website/main.js
@@ -0,0 +1,55 @@
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+  };
+  
+  ScrollReveal().reveal(".header-cont h1", {
+    ...scrollRevealOption,
+    reset: true
+
+  });
+  
+  ScrollReveal().reveal(".header__form", {
+    ...scrollRevealOption,
+    delay: 500,
+    reset: true
+
+  });
+0.
+
+  ScrollReveal().reveal(".trending__card", {
+    ...scrollRevealOption,
+    interval: 500,
+    reset: true
+
+  });
+  
+  ScrollReveal().reveal(".destination__card", {
+    duration: 1000,
+    interval: 500,
+    reset: true
+
+  });
+  
+  ScrollReveal().reveal(".seller__card", {
+    ...scrollRevealOption,
+    interval: 500,
+    reset: true
+
+  });
+  
+  ScrollReveal().reveal(".guide__card", {
+    ...scrollRevealOption,
+    interval: 500,
+    reset: true
+
+  });
+  
+  ScrollReveal().reveal(".client__card", {
+    ...scrollRevealOption,
+    interval: 500,
+    reset: true
+
+  });
+
